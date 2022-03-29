9355ee2c43ed4e2d04801e87409c56d83dde4d21
GoncaloAlpalhao <aluno23048@ipt.pt>
2022-03-29 17:22:29 +0100

    Adição de anotações

--- Vets/Vets/Models/Donos.cs
+++ Vets/Vets/Models/Donos.cs
@@ -1,20 +1,42 @@
-﻿namespace Vets.Models
-{
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vets.Models{
+
+    /// <summary>
+    /// Representa os dados do Dono de um Animal
+    /// </summary>
     public class Donos
     {
         public Donos()
         {
             ListaAnimais = new HashSet<Animais>();
         }
-
+        /// <summary>
+        /// PK para a tabela dos Donos
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Nome do Donos do animal
+        /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
         public string Nome { get; set; }
 
+        /// <summary>
+        /// NIF do dono do animal
+        /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
         public string NIF { get; set; }
 
+        /// <summary>
+        /// sexo do dono
+        /// Ff - feminino; Mm - masculino
+        /// </summary>
         public string Sexo { get; set; }
 
+        /// <summary>
+        /// lista dos animais quem é o dono
+        /// </summary>
         public ICollection<Animais> ListaAnimais { get; set; }
     }
 }
